e2de8ba4c5d19d9b04b3f008adc5828fbec8d205
Tharunasree-pasupuleti <108290257+Tharunasree-pasupuleti@users.noreply.github.com>
2022-07-25 16:28:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ import streamlit as st
 import joblib
 model = joblib.load('Social_Network_Ads')
 st.title('SOCIAL_NETWORK_ADS CLASSIFIER')
-ip = st.text_input('Enter your Message')
+ip = st.text_input('Enter the Message')
 op = model.predict([ip])
 if st.button('predict'):
   st.title(op[0])
                        
                                      
-                                                               
-                                          
-                                                     
-           
-
-          
-          
-                         
+                                                                               
+                                                
+                                                           
+                 
+      
+               
+              
+                              
