34ef12959ef3305bf345c7976502c8646e2abbd8
Tharunasree-pasupuleti <108290257+Tharunasree-pasupuleti@users.noreply.github.com>
2022-07-25 12:57:52 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import joblib
-model - joblib.load('Social_Network_Ads')
-st.title('Social_Network_Ads CLASSIFIER')
-ip = st.text_input('Enter Your Message')
-op = model.predict([ip])
-if st.button('predict'):
-  st.title(op[0])
-        
