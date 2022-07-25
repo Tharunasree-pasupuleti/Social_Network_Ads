ff98fcabbbd54525565c73f7612f724252dfa897
Tharunasree-pasupuleti <108290257+Tharunasree-pasupuleti@users.noreply.github.com>
2022-07-25 17:03:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,4 @@ op = model.predict([ip])
 if st.button('predict'):
   st.title(op[0])
                                                 
-                                                    
-                                                                               
-                                                
-                                                                 
-                       
-            
-                    
-                   
-                                   
-       
+                  
